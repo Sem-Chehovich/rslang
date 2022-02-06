ca3b6ee05de43a604cb15aac13cff3e9601bb14f
MadinaN12 <nishanbaeva.m.s.95@gmail.com>
2022-02-06 21:27:40 +0500

    feat: add requests to the server

---
+++ my-app/src/authorization/api.tsx
@@ -0,0 +1,53 @@
+const url = 'https://rs-lang-rs-school.herokuapp.com';
+
+const userLogUp = `${url}/users`;
+const userLogIn = `${url}/signin`;
+
+export interface IUser {
+  name?: string,
+  email: string,
+  password: string
+}
+
+export async function signUpUser(user: IUser) {
+  const response = await fetch(userLogUp, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(user)
+  }).catch();
+
+  if (!response.ok) return response.status;
+  const result = await response.json();
+
+  return result;
+};
+
+export async function signInUser(user: IUser) {
+  const response = await fetch(userLogIn, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(user)
+  }).catch();
+
+  if (!response.ok) {
+    localStorage.removeItem('userName');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('userToken');
+    return response.status;
+  }
+
+  const result = await response.json();
+
+  localStorage.setItem('userName', String(result.name));
+  localStorage.setItem('userId', String(result.userId));
+  localStorage.setItem('userToken', String(result.token));
+
+  return result;
+};
+  

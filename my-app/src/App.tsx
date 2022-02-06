4b97414e0183c40b94357c1fd2c63137c7507a68
Sem-Chehovich <83533289+Sem-Chehovich@users.noreply.github.com>
2022-02-06 19:24:35 +0300

    feat: create router

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,14 +3,28 @@ import './App.css';
 import Footer from './main-page/footer/footer';
 import Header from './main-page/header/header';
 import Main from './main-page/main/main';
+import WordsPageContainer from './wordsPage/wordsPageContainer';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="app">
+          <Header />  
+        <main>
+          {/* <Main /> */}
+          
+            <Routes>
+              <Route  index element={<Main />} />
+              <Route path="/home"  element={<Main />} />
+              <Route path="/dictionary" element={<WordsPageContainer />} />
+            </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 

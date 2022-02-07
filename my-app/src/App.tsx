4c493cb1acf88774d9449c6b5e5e7678f9f7b702
MadinaN12 <nishanbaeva.m.s.95@gmail.com>
2022-02-07 17:43:33 +0500

    feat: resolve conflicts

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
-import AuthForm from './authorization/AuthForm';
-=======
 import Footer from './main-page/footer/footer';
 import Header from './main-page/header/header';
 import Main from './main-page/main/main';
 import WordsPageContainer from './wordsPage/wordsPageContainer';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
->>>>>>> develop
 
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="app">
-      <AuthForm />
-    </div>
-=======
     <BrowserRouter>
       <div className="app">
           <Header />  
@@ -34,7 +25,6 @@ function App() {
         <Footer />
       </div>
     </BrowserRouter>
->>>>>>> develop
   );
 }
 
